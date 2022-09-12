b28b029a9094a4f54d48101017e914d93805b3a8
teroxrr <adtdacdb@gmail.com>
2022-09-12 10:43:53 -0300

    Add remove_bad_rows function

--- pipeline.py
+++ pipeline.py
@@ -13,6 +13,23 @@ def find_null(df: pd.DataFrame, columns: list):
         if len(null_rows) > 0:
             BAD_SUMMARY[f"null_{column}"] = null_rows
 
+def remove_bad_rows(df: pd.DataFrame):
+    """Remove from the dataset the rows contained in the "BAD_SUMMARY" dictionary"""
+    
+    # Create empty set to store row numbers
+    rows_to_exclude = set()
+    for key in BAD_SUMMARY:
+        # Add row numbers to set
+        rows_to_exclude.update(BAD_SUMMARY[key])
+    
+    # Create "bad" dataframe
+    bad = df.iloc[list(rows_to_exclude)]
+    
+    # Remove rows from dataset
+    updated_df = df.drop(rows_to_exclude)
+    
+    return updated_df, bad
+
 
 # Dictionary to hold record of excluded rows
 BAD_SUMMARY = {}
